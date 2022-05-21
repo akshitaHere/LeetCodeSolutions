b2113dc9271f04fdef4cdd91cf8518fc641cb219
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-05-21 09:55:08 -0700

    Time: 116 ms (43.80%), Space: 15.2 MB (49.50%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashy = {}
+        for index, ele in enumerate(nums):
+            diff = target - ele
+            if diff in hashy:
+                return [hashy[diff], index]
+            hashy[ele] = index
