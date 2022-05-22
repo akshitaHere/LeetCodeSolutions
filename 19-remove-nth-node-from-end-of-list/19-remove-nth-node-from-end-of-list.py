702e676a654f148975b245998185b04dd5c9a4bb
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-05-21 19:23:56 -0700

    Time: 32 ms (92.78%), Space: 13.9 MB (20.09%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -13,12 +13,13 @@ class Solution:
         while cnt < n+1:
             cnt += 1
             fast = fast.next
-        #print(fast)
+
         while fast:
             fast = fast.next
             slow = slow.next
             prev = prev.next
-        #print(prev,'\n',slow,'\n',fast)
+        
         prev.next = slow.next
         slow = slow.next
+        
         return dummy.next
