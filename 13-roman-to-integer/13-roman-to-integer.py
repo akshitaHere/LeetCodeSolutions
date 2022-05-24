fae11cdd2c30061bfffe5211f142531ec9b544f6
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-05-24 12:52:58 -0700

    Time: 70 ms (43.58%), Space: 14 MB (30.94%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romey = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        #print(romey['X'])
+        total = 0
+        for i in range(len(s)):
+            #print(romey[s[i]])
+            if i == len(s) - 1:
+                total += romey[s[i]]
+            elif romey[s[i]] < romey[s[i+1]]:
+                total -= romey[s[i]]
+            else:
+                total += romey[s[i]]
+        return total
+        
