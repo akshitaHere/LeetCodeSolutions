8e465204abcff16734089a0dbf6f8aa6a3f26f6e
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-05-24 13:14:44 -0700

    Time: 554 ms (54.51%), Space: 26 MB (30.21%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashy = {}
+        for i in nums:
+            if i not in hashy:
+                hashy[i] = 1
+            else:
+                return True
+      
+            
