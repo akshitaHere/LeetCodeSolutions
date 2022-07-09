bd01cfefa478120a9841e1a498e8ce4b47c0c3f7
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-07-09 09:38:01 -0700

    Time: 144 ms (5.63%), Space: 14.5 MB (46.00%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,14 +1,42 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        lower = s.lower()
-        a = ''.join(filter(str.isalnum,lower))
-        print(a)
-        return a == a[::-1]
-            
-         
-     
+        #Solution 1
+        #lower = s.lower()
+        #a = ''.join(filter(str.isalnum,lower))
+        #print(a)
+        #return a == a[::-1]
         
+        #Solution no workey, why?
+        #l, r = 0, len(s)-1
+        #for i in range(len(s)):
+        #    print(s[l], s[r])
+        #    if not s[l].isalnum():
+        #        l += 1
+        #    if not s[r].isalnum():
+        #        r -= 1
+        #    if s[l].lower() == s[r].lower():
+        #        l += 1
+        #        r -= 1
+        #    else:
+        #        return False
+        #return True
         
+        #Solution 3
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not self.isIsalnum(s[l]):
+                l += 1
+            while r > l and not self.isIsalnum(s[r]):
+                r -= 1    
+            if not s[l].lower() == s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
         
+    def isIsalnum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
         
        
