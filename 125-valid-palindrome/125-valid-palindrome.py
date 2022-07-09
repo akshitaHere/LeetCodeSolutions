0fd01f63d3116d0a951d6f661a26503315f9caf5
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-07-09 09:09:51 -0700

    Time: 49 ms (90.69%), Space: 15.2 MB (29.43%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,9 +3,7 @@ class Solution:
         lower = s.lower()
         a = ''.join(filter(str.isalnum,lower))
         print(a)
-        rev = a[::-1]
-        print(rev)
-        return a == rev
+        return a == a[::-1]
             
          
      
