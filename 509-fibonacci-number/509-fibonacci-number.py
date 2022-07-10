ff4be98578c908cecc64912b579554785e055d85
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-07-10 12:02:44 -0700

    Time: 59 ms (38.73%), Space: 13.9 MB (53.83%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
+        #Solution 1
+        #Time : O(n), Space : 1
         f0, f1 = 0, 1
         if n == 0: return 0
         if n == 1: return 1
@@ -11,3 +13,15 @@ class Solution:
             f1 = fi
             
         return fi
+    
+        #Solution 2
+        #Time : O(n), Space : O(n)
+        f = []
+        f.append(0)
+        f.append(1)
+        
+        for i in range(2, n+1):
+            f.append(f[i-1] + f[i-2])
+        
+        return f[n]
+    
