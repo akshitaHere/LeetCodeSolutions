2af8a1cb735d9d31f0c24fa45cfd757d986ded08
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-07-22 12:24:50 -0700

    Time: 473 ms (5.00%), Space: 18.7 MB (70.57%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -7,9 +7,7 @@ class Solution:
                 hashy[i] = 1
             else:
                 hashy[i] += 1
-        print(hashy)
-        #res = dict(sorted(hashy.items(), key = itemgetter(1), reverse = True))
-        #return res
+                
         for i in range(k):
             maxVal = max(hashy.keys(), key=(lambda k: hashy[k]))
             res.append(maxVal)
