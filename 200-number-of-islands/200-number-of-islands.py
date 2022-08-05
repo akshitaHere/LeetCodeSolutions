d3c3862abba58954d61cd66da3f19ba04a5111bb
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-08-04 21:38:59 -0700

    Time: 531 ms (40.18%), Space: 21.9 MB (19.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -24,8 +24,6 @@ class Solution:
                         visited.add((r, c))
                         queue.append((r, c))
                         
-                    
-        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visited:
