f752376e45fc5823e8661246130a481641a78938
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-08 11:14:24 -0700

    Time: 225 ms (15.21%), Space: 19.6 MB (26.31%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,32 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #Time : O(n), Space : O(n)
-        count = {}
+        hashy = {}
         freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            hashy[n] = 1 + hashy.get(n, 0)
+        for n, c in hashy.items():
+            freq[c].append(n)
         
-        for i in nums:
-            count[i] = 1 + count.get(i, 0)
-        for keyNum, valueCount in count.items():
-            freq[valueCount].append(keyNum)
         res = []
-        for i in range(len(freq) - 1, 0 , -1):
+        for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
-            if len(res) == k:
-                return res
-
-        
-        
-        
-        
-#        res = []
-#       hashy = {}
-#        for i in nums:
-#            hashy[i] = 1 + hashy.get(i, 0)
-#            
-#        for i in range(k):
-#            maxVal = max(hashy.keys(), key=(lambda k: hashy[k]))
-#            res.append(maxVal)
-#            hashy.pop(maxVal)
-#        
-#        return res
+                if len(res) == k:
+                    return res
+            
+            
