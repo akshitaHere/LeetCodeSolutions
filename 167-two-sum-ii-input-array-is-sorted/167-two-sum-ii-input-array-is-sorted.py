cf694149eb3f1f6c2239e9fc266729ea66b24b10
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-08 11:38:19 -0700

    Time: 316 ms (7.17%), Space: 14.9 MB (41.21%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hashy = {}
-        for index, ele in enumerate(numbers):
-            diff = target - ele
-            if diff in hashy:
-                return hashy[diff] + 1, index + 1
-            hashy[ele] = index
+        
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            if numbers[r] + numbers[l] > target:
+                r -= 1
+            elif numbers[r] + numbers[l] < target:
+                l += 1
+            else:
+                return l+1, r+1
