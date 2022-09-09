eb2b7f62054755cf50b66f2c15cfcb216651f3c2
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-09 09:57:38 -0700

    Time: 300 ms (9.83%), Space: 14.9 MB (41.20%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -4,20 +4,12 @@ class Solution:
         #Space : O(1) , Time : O(n)
         l, r = 0, len(numbers) - 1
         while l < r:
-            if numbers[r] + numbers[l] > target:
+            if numbers[l] + numbers[r] == target:
+                return l + 1, r + 1
+            elif numbers[l] + numbers[r] > target:
                 r -= 1
-            elif numbers[r] + numbers[l] < target:
-                l += 1
             else:
-                return l+1, r+1
+                l += 1
             
-            
-#        Solution 2: Space : O(n), Time : O(n)        
-#        hashy = {}
-#        for index, ele in enumerate(numbers, 1):
-#            diff = target - ele
-#            if diff in hashy:
-#                return [hashy[diff], index]
-#            hashy[ele] = index
-
+        
 
