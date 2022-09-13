582dd1f1d3d59042a168d5a4167012cfd18ffbd8
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-13 02:23:56 -0700

    Time: 178 ms (12.18%), Space: 17.4 MB (52.75%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,5 +1,6 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #Time : O(n)
         res = []
         for i in range(len(intervals)):
             if newInterval[1] < intervals[i][0]: #enter first
