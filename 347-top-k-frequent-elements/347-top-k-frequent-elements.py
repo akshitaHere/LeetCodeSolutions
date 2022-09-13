a5f9e3e023a58063f360c8d222cd5ad1c8208ea1
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-13 00:59:14 -0700

    Time: 241 ms (13.00%), Space: 19.5 MB (28.63%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,29 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #Time : O(n), Space : O(n)
+        #Time : O(n)
+        res = []
         count = {}
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        
         freq = [[] for i in range(len(nums) + 1)]
         
-        for i in nums:
-            count[i] = 1 + count.get(i, 0)
-        for keyNum, valueCount in count.items():
-            freq[valueCount].append(keyNum)
-        res = []
-        for i in range(len(freq) - 1, 0 , -1):
-            for n in freq[i]:
-                res.append(n)
+        for key, value in count.items():
+            freq[value].append(key)
+        
+        for i in range(len(freq) - 1, 0, -1):
+            for f in freq[i]:
+                res.append(f)
             if len(res) == k:
                 return res
-
-#Solution 2        
-#        res = []
-#       hashy = {}
-#        for i in nums:
-#            hashy[i] = 1 + hashy.get(i, 0)
-#            
-#        for i in range(k):
-#            maxVal = max(hashy.keys(), key=(lambda k: hashy[k]))
-#            res.append(maxVal)
-#            hashy.pop(maxVal)
-#        
-#        return res
