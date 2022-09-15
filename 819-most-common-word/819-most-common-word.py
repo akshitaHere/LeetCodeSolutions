4fca3023905a9a5f9f25e0a604f0f55902ee98ae
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-09-14 22:22:29 -0700

    Time: 83 ms (10.28%), Space: 13.9 MB (37.83%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -5,5 +5,6 @@ class Solution:
         for c in "!?',;.": paragraph = paragraph.replace(c, " ")
         for word in paragraph.lower().split():
             if word not in banned:
+                print(word)
                 hashy[word] = 1 + hashy.get(word, 0)
         return(max(hashy, key = hashy.get))
