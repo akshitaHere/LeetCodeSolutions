0442bb91ca907d986c6f440ffa92df36f8f880e2
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-10-17 16:31:50 -0700

    Time: 114 ms (41.28%), Space: 14.1 MB (37.95%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -3,7 +3,6 @@ class Solution:
         counter = {"U" : 0, "D" : 0, "R" : 0, "L" : 0 }
         for m in moves:
             counter[m] = 1 + counter.get(m, 0)
-        print(counter["U"], counter["D"], counter["R"], counter["L"])
         if (abs(counter["U"] - counter["D"]) + abs(counter["L"] - counter["R"])) == 0:
             return True
         else:
