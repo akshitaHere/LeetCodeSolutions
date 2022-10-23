7a76b19cebc8bb54ad3750db01144beedf235ac8
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-10-23 12:39:04 -0700

    Time: 1543 ms (29.30%), Space: 25.2 MB (99.65%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,6 +3,7 @@ class Solution:
         #Do again using leet code's solution
         #Time: O(k log n)
         heapq._heapify_max(nums)
+        print(nums)
         while k > 0:
             a = heapq._heappop_max(nums)
             k -= 1
