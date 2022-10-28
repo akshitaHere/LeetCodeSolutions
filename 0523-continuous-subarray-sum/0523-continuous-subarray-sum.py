82fbcdb4aeb24ba8806136d01679a251c6bcee06
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-10-28 15:57:44 -0700

    Time: 2691 ms (19.74%), Space: 33.2 MB (69.82%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        remainders = {0: -1} # we can get a remainder of 0 with a subarray containing no elements
-        curr_rem = 0 # current remainder
-        for i,num in enumerate(nums):
-            curr_rem = (curr_rem+num)%k # update current remainder
-            if curr_rem in remainders: # if we have seen this remainder
-                if remainders[curr_rem] < i-1: # if we have seen this remainder before but it was not at the previous prefix (since we need our subarray to have length 2)
-                    return True
-            else: # if we haven't seen the remainder
-                remainders[curr_rem] = i # add this remainder and the index it is at
+        #Prefix sum
+        #Time : O(n), Space : O(n)
+        remainder = {0: -1} #remainder : index
+        total = 0
+        for index, ele in enumerate(nums):
+            total += ele
+            r = total % k
+            if r not in remainder:
+                remainder[r] = index
+            elif index - remainder[r] > 1:
+                return True
         return False
