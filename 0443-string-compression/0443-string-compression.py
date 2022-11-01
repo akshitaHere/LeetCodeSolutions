52f270a5f98d5988309aea49ce88c53fcf863be5
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-11-01 12:15:08 -0700

    Time: 127 ms (49.13%), Space: 13.9 MB (73.17%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,17 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        l, r = 0, 0
-        while r < len(chars):
-            chars[l] = chars[r]
+        walker, runner = 0, 0
+        while runner < len(chars):
+		
+            chars[walker] = chars[runner]
             count = 1
-            while r + 1 < len(chars) and chars[r] == chars[r+1]:
-                r += 1
+			
+            while runner + 1 < len(chars) and chars[runner] == chars[runner+1]:
+                runner += 1
                 count += 1
 			
             if count > 1:
                 for c in str(count):
-                    chars[l+1] = c
-                    l += 1
-            r += 1
-            l += 1
-        return l
+                    chars[walker+1] = c
+                    walker += 1
+            runner += 1
+            walker += 1
+        print(walker)
+        return walker
