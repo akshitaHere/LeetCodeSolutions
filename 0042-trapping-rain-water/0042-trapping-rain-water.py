a51bd703a40488db7643482a38a1afc150e56963
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-11-01 10:12:10 -0700

    Time: 129 ms (93.95%), Space: 16.1 MB (47.20%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,11 +1,12 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        
-        while not height:
+        #Time : O(n), Space : O(1)
+        if not height:
             return 0
+        
         l, r = 0, len(height) - 1
-        res = 0
         maxL, maxR = height[l], height[r]
+        res = 0
         
         while l < r:
             if maxL <= maxR:
@@ -17,18 +18,3 @@ class Solution:
                 maxR = max(maxR, height[r])
                 res += maxR - height[r]
         return res
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-        
