39079179e2c0aabd0c761ff2358dda484451a01a
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-10-31 22:12:56 -0700

    Time: 311 ms (83.35%), Space: 15.3 MB (74.47%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
@@ -1,3 +1,9 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
+        #Time : O(n), Space : O(1)
         return sum(nums) - len(nums) * min(nums)
+        #nums.sort()
+        #res = 0
+        #for i in range(len(nums) - 1, -1, -1):
+        #    res += nums[i] - nums[0]
+        #return res
