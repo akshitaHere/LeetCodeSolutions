2b9b5a8a74cf3539313a086876ac519355c8fc60
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-11-01 12:38:01 -0700

    Time: 111 ms (82.63%), Space: 16.6 MB (28.79%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -2,17 +2,19 @@ class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         count = Counter(nums)
         nums = sorted(list(set(nums)))
+        earn1, earn2 = 0 ,0
+        #[1, 2, 3]
+        #e1, e2, i
+        #    e1, e2
         
-        earn1, earn2 = 0, 0
         for i in range(len(nums)):
             curEarn = nums[i] * count[nums[i]]
-            #not adding 2 adjacent vallues
+            #can't use both cur value and prev value
             if i > 0 and nums[i] == nums[i - 1] + 1:
                 temp = earn2
-                earn2 = max(curEarn + earn1, earn2)
+                earn2 = max(earn2, earn1 + curEarn)
                 earn1 = temp
-            else: 
-            #adding previous value as well
+            else:
                 temp = earn2
                 earn2 = curEarn + earn2
                 earn1 = temp
