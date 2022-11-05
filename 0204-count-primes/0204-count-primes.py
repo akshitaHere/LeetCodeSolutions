ce69a1d553f9d9acd416bd83455938173404d49f
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-11-04 23:42:58 -0700

    Time: 5068 ms (57.08%), Space: 52.8 MB (84.82%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,10 +3,11 @@ class Solution:
         if n < 2:
             return 0
         primes = [True] * n
+        
         primes[0] = primes[1] = False
+        
         for num in range(2, n):
             if primes[num]:
                 for multiple in range(num**2, n, num):
                     primes[multiple] = False
-                
         return sum(primes)
