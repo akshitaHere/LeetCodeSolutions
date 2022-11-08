a9a740a520a8459b3a3a857ff8bf531ea2c7ed19
akshitaHere <99557340+akshitaHere@users.noreply.github.com>
2022-11-08 08:37:34 -0800

    Time: 757 ms (28.65%), Space: 13.9 MB (9.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,23 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        #Solution 1 : Iterative
-        #Time : O(n), Space : 1
         if n <= 1:
             return n
-        
-        f0, f1 = 0, 1
-        for i in range(2, n + 1):
-            fi = f0 + f1
-            f0 = f1
-            f1 = fi
-        
-        return f1
-    
-        #Solution 2 : Dp recursive
-        #Time : O(n), Space : O(n)
-        #f = []
-        #f.append(0)
-        #f.append(1)
-        #for i in range(2, n+1):
-        #    f.append(f[i-1] + f[i-2])
-        #return f[n]
+        return self.fib(n - 1) + self.fib(n - 2)
